8806f53b2dadf4083784cb115019fadaac6b0d86
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2021-01-03 17:32:17 +0400

    add delete handle

--- app/src/main/java/com/example/todoapplication/MainActivity.kt
+++ app/src/main/java/com/example/todoapplication/MainActivity.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import android.hardware.input.InputManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.SparseBooleanArray
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
+import androidx.core.view.get
 import android.widget.ListView as ListView
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val addButton = findViewById<Button>(R.id.add_button)
-        val deleteButton = findViewById<Button>(R.id.add_button)
+        val deleteButton = findViewById<Button>(R.id.delete_button)
 
 
         val toDoList = findViewById<ListView>(R.id.to_do_list_view)
@@ -23,18 +25,19 @@ class MainActivity : AppCompatActivity() {
         val errorMessageText = findViewById<TextView>(R.id.error_message_text)
 
         val listItems = arrayListOf<String>()
-        val arrayAdapter: ArrayAdapter<*>
-        arrayAdapter = ArrayAdapter(this, android.R.layout.select_dialog_multichoice, listItems)
+        val arrayAdapter: ArrayAdapter<String> =
+            ArrayAdapter(this, android.R.layout.select_dialog_multichoice, listItems)
         toDoList.adapter = arrayAdapter
 
+//        add item
         addButton.setOnClickListener {
             val itemText: String = addItemEdit.text.toString()
-            if (!itemText.equals("") && itemText.length >= 3) {
+            if (itemText.isNotEmpty()) {
                 listItems.add(itemText)
                 arrayAdapter.notifyDataSetChanged()
                 addItemEdit.setText("")
                 errorMessageText.visibility = View.GONE
-                Toast.makeText(this, itemText + " added", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, itemText + " added", Toast.LENGTH_SHORT).show()
             }  else {
                 errorMessageText.visibility = View.GONE
                 errorMessageText.text = "Please, write something..."
@@ -45,42 +48,19 @@ class MainActivity : AppCompatActivity() {
                   imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
         }
 
-//        addButton.setOnClickListener { addItemButton(it) }
-//        deleteButton.setOnClickListener { deleteItemButton(it) }
+//        delete item
+        deleteButton.setOnClickListener {
+            val position: SparseBooleanArray = toDoList.checkedItemPositions
+            val count = toDoList.count
+
+            for(item in count - 1 downTo 0) {
+                if(position.get(item)) {
+                    arrayAdapter.remove(listItems.get(item))
+                }
+            }
+            position.clear()
+            arrayAdapter.notifyDataSetChanged()
+        }
+
     }
-
-//    private fun addItemButton(view: View) {
-//
-//        val toDoList = findViewById<ListView>(R.id.to_do_list_view)
-//        val addItemEdit = findViewById<EditText>(R.id.add_item_edit)
-//        val errorMessageText = findViewById<TextView>(R.id.error_message_text)
-//
-//        val listItems = arrayListOf<String>()
-//        val arrayAdapter: ArrayAdapter<*>
-//        arrayAdapter = ArrayAdapter(this, android.R.layout.select_dialog_multichoice, listItems)
-//        toDoList.adapter = arrayAdapter
-//
-//        val itemText: String = addItemEdit.text.toString()
-//        if (!itemText.equals("") && itemText.length >= 3) {
-//            listItems.add(itemText)
-//            arrayAdapter.notifyDataSetChanged()
-//            addItemEdit.setText("")
-//            Toast.makeText(this, itemText + " added", Toast.LENGTH_LONG).show()
-//        } else {
-//            errorMessageText.visibility = View.GONE
-//            errorMessageText.text = "Please, write something..."
-//            errorMessageText.visibility = View.VISIBLE
-//            Toast.makeText(this, "Please, fill the gap", Toast.LENGTH_LONG).show()
-//        }
-//
-//        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-//        imm.hideSoftInputFromWindow(view.windowToken, 0)
-//    }
-
-//    private fun deleteItemButton(view: View) {
-//        val toDoList = findViewById<ListView>(R.id.to_do_list_view)
-//        toDoList.setOnItemClickListener((adapterView, view, position, id) -> {
-//
-//        })
-//    }
 }
